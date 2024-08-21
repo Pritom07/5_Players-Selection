ade105da57424cfed492f9ce785d8d4991f5a45c
Pritom07 <pritommohajon4545@gmail.com>
2024-08-21 12:17:57 +0600

    player selection process full complite

--- js/script.js
+++ js/script.js
@@ -5,7 +5,34 @@ document.addEventListener("DOMContentLoaded", function () {
             const targetEvent = event.target;
             const targetParent = targetEvent.parentNode;
             const playerName = targetParent.children[0].innerText;
-            addUlElement(playerName, targetEvent);
+            const liCount = addUlElement(playerName, targetEvent);
+            const removeButton = document.getElementById("remove");
+            if (liCount >= 0) {
+                removeButton.removeAttribute("disabled");
+                removeButton.style.backgroundColor = "blue";
+            }
         })
+    })
+})
+
+document.getElementById("remove").addEventListener("click", function () {
+    const ulId = document.getElementById("fiveplayersfield");
+    const liCount = ulId.children.length;
+    const removeButton = document.getElementById("remove");
+    if (liCount == 1) {
+        removeButton.setAttribute("disabled", "true");
+        removeButton.style.backgroundColor = "grey";
+    }
+    const getRemoved = ulId.children[liCount - 1];
+    ulId.removeChild(getRemoved);
+    const selectButtons = document.querySelectorAll(".card-body button");
+    selectButtons.forEach((button) => {
+        const playerNameTag = button.parentNode.children[0];
+        const playerName = playerNameTag.textContent;
+        const removePlayerName = getRemoved.textContent;
+        if (playerName == removePlayerName) {
+            button.removeAttribute("disabled");
+            button.style.backgroundColor = "blue";
+        }
     })
 })
--- js/utilities.js
+++ js/utilities.js
@@ -1,6 +1,6 @@
 function addUlElement(playerName, targetEvent) {
     const ulId = document.getElementById("fiveplayersfield");
-    const listcount = ulId.children.length
+    const listcount = ulId.children.length;
     if (listcount > 4) {
         alert("You can't choose more than 5 players. It's a 5 players game, you know!!");
         return;
@@ -12,5 +12,6 @@ function addUlElement(playerName, targetEvent) {
         const button = targetEvent;
         button.setAttribute("disabled", "true");
         button.style.backgroundColor = "grey";
+        return listcount;
     }
 }
